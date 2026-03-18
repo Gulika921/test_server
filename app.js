d47f3db13537bc90fa9b5b4fa7c723bc340a97f8
gulika921@gmail.com <gulika921@gmail.com>
2026-03-18 14:51:45 +0100

    URL changed

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 const app = express();
+const URL = "100.86.121.25";
+const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello from Raspberry Pi 🚀");
 });
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${URL}:${PORT}`);
 });
